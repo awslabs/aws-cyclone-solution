3bba4fbe142118bbbec5842cb77b09b58a487969
Ludvig Nordstrom <ludvn@amazon.co.uk>
2022-07-29 12:38:10 +0100

    hosts update

--- cyclone-cli/commands/hosts.py
+++ cyclone-cli/commands/hosts.py
@@ -100,7 +100,7 @@ def get_vpcs(ctx, param, value):
         for vpc in vpcs['Vpcs']:
             click.echo('----------------------------')
             if vpc['IsDefault'] == True:
-                click.echo('RECOMMENDED')
+                click.echo('DEFAULT VPC')
             click.echo(vpc['VpcId'])
             click.echo(vpc['CidrBlock'])
             click.echo('Default: ' + str(vpc['IsDefault']))
