d4767e931a56ab0a21c02b71313e5289dd422490
Jamie Baldanza <jbaldanza@relaytx.com>
2022-10-18 14:02:48 -0400

    chore: support manual ecr image replication

--- hyper_batch/hyper_job_definitions.py
+++ hyper_batch/hyper_job_definitions.py
@@ -103,7 +103,10 @@ class JobDefinitions(core.Stack):
             if job_definition['use_cyclone_image'] == "True":
                 image_uri = '{}.dkr.ecr.{}.amazonaws.com/{}:{}'.format(self.account, self.region, stack_name, job_definition['cyclone_image_name'])
             else:
-                image_uri = job_definition['image_uri']
+                if "$REGION" in str(job_definition['image_uri']):
+                    image_uri = str(job_definition['image_uri']).replace("$REGION", self.region)
+                else:
+                    image_uri = job_definition['image_uri']
 
             container = ecs.ContainerImage.from_registry(image_uri)
 
